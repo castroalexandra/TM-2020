cdbfc2e028c6406beaea2d270c27af2778fcdfc2
Alexandra Castro <alexandracastro@ipvc.pt>
2020-03-02 15:58:29 +0000

    AlexandraCastro-20764

---
+++ Aula 2/exercicio1/AlexandraCastro-20764/script.js
@@ -0,0 +1,11 @@
+function lerInput() {
+    var texto = document.getElementById(  "numero").value;
+    if(!texto.length) alert( "Sem Conteudo no Input");
+    alert("texto");
+
+    document.getElementById("texto").innerText = texto
+
+
+
+
+}
