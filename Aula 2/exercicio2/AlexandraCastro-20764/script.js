8ab444a743386e363549ab0907b9d25b90a7c81e
Alexandra Castro <alexandracastro@ipvc.pt>
2020-03-02 16:41:20 +0000

    AlexandraCastro-20764

---
+++ Aula 2/exercicio2/AlexandraCastro-20764/script.js
@@ -0,0 +1,15 @@
+var arrayNumbers =[];
+function adicionarNumero() {
+    var numero = document.getElementById("numbersId").value;
+   arrayNumbers.push(parseInt(numero));
+    document.getElementById("NumerosAdicionados").innerText = arrayNumbers.toString();
+
+
+}
+function calcularNumero() {
+    if ( arrayNumbers.length>5) {
+        var maiorNumero = Math.max.apply(Math, arrayNumbers);
+        alert(maiorNumero);
+
+    }
+}
